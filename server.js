00c5bb53142b62c5ddd1e885dac7095144c01c02
zacboz <zacboz@gmail.com>
2017-02-13 12:43:16 -0700

    framework for server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const massive = require('massive');
+const session = require('express-session');
+const config = require('./config')
+const port = 3050;
+
+const app = module.exports = express();
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+// console.log(__dirname);
+app.use(session({
+  secret: config.sessionSecret,
+  resave: false,
+  saveUninitialized: false
+}));
+
+const conn = massive.connectSync({
+  connectionString : "postgres://postgres:@localhost/kombuchadog-clone"
+});
+app.set('db', conn);
+const db = app.get('db');
+const productsController = require('./productsController');
+
+app.get('/shop', productsController.getShop);
+
+
+app.listen(3050, () => {
+  console.log('Connected on port', port)
+});
