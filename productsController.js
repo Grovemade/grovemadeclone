7e3391fea15a06e91a4c3d71e2e63765ff3481bc
zacboz <zacboz@gmail.com>
2017-02-17 17:40:01 -0700

    got about endpoint working back to front

--- productsController.js
+++ productsController.js
@@ -3,6 +3,18 @@ const db = app.get('db');
 
 module.exports = {
 
+getAboutPage: (req, res) => {
+  db.read_aboutPage((err, response) => {
+    if(err) {
+      console.log(err);
+      res.status(200).json(err);
+    } else {
+      console.log('getting jounral', response);
+      res.status(200).json(response);
+    }
+  });
+},
+
 getJournal: (req, res) => {
   db.read_journal((err, response) => {
     if(err) {
@@ -14,6 +26,8 @@ getJournal: (req, res) => {
     }
   });
 },
+
+
 
 
 };
